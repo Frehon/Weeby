1acf0ba77b09eca37b9e378bae9ef15b383e491f
Gogol <tomasz.gogolewski1993@gmail.com>
2019-08-16 10:54:23 +0200

    Login component created

--- weeby-client/src/app/app.module.ts
+++ weeby-client/src/app/app.module.ts
@@ -5,14 +5,16 @@ import {AppComponent} from './app.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatButtonToggleModule, MatCardModule, MatIconModule} from '@angular/material';
-import { PostComponent } from './post/post.component';
-import {PostRestService} from './logic/post.rest.service';
+import { PostComponent } from './usecase/post/post.component';
+import {PostRestService} from './usecase/logic/post.rest.service';
 import {HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './authentication/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
