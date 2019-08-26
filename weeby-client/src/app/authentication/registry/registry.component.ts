8bd73a04d2f55ef34ace167ff484b87af05bf293
tgogolewski <tomasz.gogolewski@hotmail.com>
2019-08-26 15:02:51 +0200

    Registry component first draft

--- weeby-client/src/app/authentication/registry/registry.component.ts
+++ weeby-client/src/app/authentication/registry/registry.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-registry',
   templateUrl: './registry.component.html',
-  styleUrls: ['./registry.component.css']
+  styleUrls: ['./registry.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class RegistryComponent implements OnInit {
 
